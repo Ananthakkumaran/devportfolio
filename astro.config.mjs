6e356e651f703ce7176bfe542874242c31d20c3d
ananth77 <p.ananthakkumaran@gmail.com>
2026-03-16 19:37:33 +0800

    config
    Updated config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  base: "/devportfolio/",  // <--- important for GitHub Pages
   vite: {
     plugins: [tailwindcss()],
   },
